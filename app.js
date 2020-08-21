7153bf0845809eda1952c36d0c4306d002c8834d
Richard Brower <richardtia@gmail.com>
2020-08-20 21:24:49 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin:A0nEgkSBXz8vBtiI@cluster0.msuwb.mongodb.net/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const dbSchema = {
+  title: String,
+  body: String
+};
+
+const Entry = mongoose.model("Entry", dbSchema);
+
+let posts = [];
+
+app.get("/", function(req, res) {
+
+  Entry.find({}, function(err, foundEntries) {
+    res.render("home", {
+      firstParagraph: homeStartingContent,
+      allPosts: foundEntries
+    });
+  });
+
+
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutPageInfo: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactPageInfo: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  const newTitle = req.body.postTitle;
+  const newBody = req.body.postBody;
+
+  const entry = new Entry ({
+    title: newTitle,
+    body: newBody
+  });
+
+  entry.save();
+
+  res.redirect("/");
+});
+
+app.get("/posts/:id", function(req, res) {
+
+  const postId = req.params.id;
+
+  Entry.findOne({"_id": postId}, function(err, foundPost) {
+    res.render("post", {
+      pageTitle: foundPost.title,
+      pageContent: foundPost.body
+    });
+  });
+
+
+});
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port === null || port == "") {
+  port = 3000;
+};
+
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
